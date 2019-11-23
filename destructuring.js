c651c34fa405714216f41245b74478038e5fa4f2
wholeop3 <wholeop3@gmail.com>
2019-11-22 17:08:39 -0700

    this freakin took forever and a millenia

--- destructuring.js
+++ destructuring.js
@@ -21,7 +21,7 @@ var carDetails = {
   Use object destructuring to save the property values from the object carDetails into new variables. 
 */
 
-//Code Here
+const {color,make,model,year}=carDetails
 
 
 
@@ -34,7 +34,7 @@ var carDetails = {
 */
 
 function greeting( obj ) {
-  //Code Here
+  const {firstName, lastName, title} = obj
   
   // Do not edit the code below.
   return 'Hello, ' + title + ' ' + firstName + ' ' + lastName + '!';
@@ -52,8 +52,17 @@ function greeting( obj ) {
   Use object destructuring to save the property values to new variables.
   Sum up the values and return the total number.
 */
-
-//Code Here
+  function totalPopulation (obj){
+    const {utah, california, texas, arizona} = obj
+    // const utah = obj.utah
+        return utah+california+texas+arizona
+          }
+// let obj = {
+//   utah: 7,
+//   california: 13,
+//   texas: 56,
+//   arizona: 5
+// }
 
 
 
@@ -67,10 +76,16 @@ function greeting( obj ) {
   Push these new variables to an array and return the array. 
 */
 
-//Code Here
+function ingredients (obj){
+  const {carb, fat, protein}= obj
+    let newVariable = []
+      newVariable.push(carb, fat, protein)
+    return newVariable  
+    }
 
 
 
+// 
 ////////// PROBLEM 5 //////////
 
 /*
@@ -85,7 +100,13 @@ function greeting( obj ) {
   Find the smallest number of the three and return that number.
 */
 
-//Code Here
+function largeNumbers({first, second, third}) {
+  if (first < second && first < third){
+    return first
+      } else if (second < first && second < third){
+          return second
+            }else return third
+    }
 
 
 
@@ -97,6 +118,12 @@ function greeting( obj ) {
   Find the longest array and return that array.
 */
 
-//Code Here
-
+function numberGroups({a ,b, c}){
+  if (a > b && a > c){
+    return a
+      } else if (b > a && b> c){
+        return b
+          } else return c
+      }
+ 
 
